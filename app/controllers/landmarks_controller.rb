a7bd505a8f92a427fbf75d223fdfefdde24da9ea
Samantha brake <sammy.brake@gmail.com>
2019-03-18 16:13:47 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -31,7 +31,7 @@ class LandmarksController < ApplicationController
     @landmark = Landmark.find(params[:id])
      @landmark.update(params[:landmark])
      @landmark.save
-    erb ":landmarks/#{@landmark.id}"
+    erb ":landmarks/show"
   end
 
 end
