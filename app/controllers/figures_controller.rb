7adffa0082728aad0b2a1e2912b6c02c1d43cce7
Samantha brake <sammy.brake@gmail.com>
2019-03-18 05:39:21 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,18 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/new'
+  end
+
+  post '/figures/new' do
+    @figure = Figure.create(name: params[:figure][:name])
+    params[:figure][:title_ids].each do |id|
+      @figure.titles << Title.find_or_create_by(id)
+    end
+    @figure.save
+    # binding.pry
+    erb :'figures/show'
+  end
 end
